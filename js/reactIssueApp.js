b4ff0eb0de291e53d7052da2550c61eca41948fa
Besganabes <50441759+Besganabes@users.noreply.github.com>
2021-09-18 14:08:25 +0300

    FIX: resolve pagination and search field bugs, major HTML & JS refactoring.

--- js/reactIssueApp.js
+++ js/reactIssueApp.js
@@ -1,46 +1,127 @@
 const reactIssueApp = angular.module("reactIssueApp", ["angular.filter"]);
 
-reactIssueApp.controller("reactIssueController", function ($scope, $http) {
-    $scope.Math = window.Math;
-    $scope.users = null;
+reactIssueApp.controller("reactIssueController", function ($scope, $http, $filter) {
     $scope.usersDataUrl = "https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json";
+    $scope.users = null;
     $scope.selectedUser = null;
-    $scope.searchString = "";
-    $scope.stateFilter = "";
     $scope.pagination = {
-        users: [],
         currentPage: 1,
         usersPerPage: 20,
-        maxSize: 3,
-        leftNumButtonPage: 1,
-        middleNumButtonPage: 2,
-        rightNumButtonPage: 3
-    };
+        searchString: "",
+        stateFilter: ""
+    }
 
     $scope.init = function () {
         console.log("Initialization sequence...");
         $http.get($scope.usersDataUrl).then(function (response) {
             $scope.users = response.data;
-            $scope.pagination.users = $scope.users;
-            $scope.$watch("pagination.currentPage", function () {
-                let begin = ($scope.pagination.currentPage - 1) * $scope.pagination.usersPerPage;
-                let end = begin + $scope.pagination.usersPerPage;
-
-                $scope.pagination.users = $scope.users.slice(begin, end);
-
-                $scope.pagination.leftNumButtonPage = $scope.pagination.currentPage === Math.ceil($scope.users.length / $scope.pagination.usersPerPage) ? $scope.pagination.currentPage - 2 : $scope.pagination.currentPage === 1 ? 1 : $scope.pagination.currentPage - 1;
-                $scope.pagination.middleNumButtonPage = $scope.pagination.currentPage === Math.ceil($scope.users.length / $scope.pagination.usersPerPage) ? $scope.pagination.currentPage - 1 : $scope.pagination.currentPage > 1 ? $scope.pagination.currentPage : 2;
-                $scope.pagination.rightNumButtonPage = $scope.pagination.currentPage === 1 ? $scope.pagination.currentPage + 2 : $scope.pagination.currentPage === Math.ceil($scope.users.length / $scope.pagination.usersPerPage) ? $scope.pagination.currentPage : $scope.pagination.currentPage + 1;
-            });
             console.log("Initialization sequence complete.");
         }, function (error) {
             console.error(error.statusText);
         });
     };
 
-    $scope.selectUser = function ($event, user) {
-        Array.from(document.querySelectorAll(".selected")).forEach((element) => element.classList.remove("selected"));
-        $event.currentTarget.className = "selected";
+    $scope.selectUser = function (user) {
         $scope.selectedUser = user;
+    };
+
+    $scope.removeClassFromAllElements = function (className) {
+        Array.from(document.querySelectorAll(".selected")).forEach((element) => element.classList.remove(className));
+    };
+
+    $scope.highlightSelectedRow = function ($event) {
+        $scope.removeClassFromAllElements("selected");
+        $event.currentTarget.className = "selected";
+    };
+
+    $scope.closeAdditionalInfo = function () {
+        $scope.removeClassFromAllElements("selected");
+        $scope.selectedUser = null;
+    };
+
+    $scope.getFilteredUsers = function () {
+        return $filter('filter')($scope.users, $scope.pagination.searchString);
+    };
+
+    $scope.getOffset = function () {
+        return ($scope.pagination.currentPage - 1) * $scope.pagination.usersPerPage;
+    };
+
+    $scope.previousButtonIsDisabled = function () {
+        return $scope.pagination.currentPage === 1;
+    };
+
+    $scope.nextButtonIsDisabled = function () {
+        return $scope.pagination.currentPage >= $scope.getFilteredUsers().length / $scope.pagination.usersPerPage;
+    };
+
+    $scope.selectAdjacentPage = function (forward) {
+        forward ? $scope.pagination.currentPage += 1 : $scope.pagination.currentPage -= 1;
+    };
+
+    $scope.numButtonIsActive = function (pageNumber) {
+        return $scope.pagination.currentPage === pageNumber;
+    };
+
+    $scope.selectPage = function (pageNumber) {
+        $scope.pagination.currentPage = pageNumber;
+    };
+
+    $scope.getPagesAmount = function () {
+        return Math.ceil($scope.getFilteredUsers().length / $scope.pagination.usersPerPage);
+    };
+
+    $scope.getLeftNumButtonPage = function () {
+        const pagesAmount = $scope.getPagesAmount();
+        const currentPage = $scope.pagination.currentPage;
+
+        if (pagesAmount === 1 || pagesAmount === 2 || currentPage === 1) {
+            return 1;
+        } else if (currentPage === pagesAmount) {
+            return currentPage - 2;
+        } else {
+            return currentPage - 1;
+        }
+    };
+
+    $scope.getMidNumButtonPage = function () {
+        const pagesAmount = $scope.getPagesAmount();
+        const currentPage = $scope.pagination.currentPage;
+
+        if (pagesAmount === 1) {
+            return 1;
+        } else if (pagesAmount === 2) {
+            return 2;
+        } else if (currentPage === pagesAmount) {
+            return currentPage - 1;
+        } else if (currentPage > 1) {
+            return currentPage;
+        } else {
+            return 2;
+        }
+    };
+
+    $scope.getRightNumButtonPage = function () {
+        const pagesAmount = $scope.getPagesAmount();
+        const currentPage = $scope.pagination.currentPage;
+
+        if (currentPage === 1) {
+            return currentPage + 2;
+        } else if (currentPage === pagesAmount) {
+            return currentPage;
+        } else {
+            return currentPage + 1;
+        }
+    };
+
+    $scope.$watch("pagination.searchString", function () {
+        $scope.pagination.currentPage = 1;
+    });
+});
+
+reactIssueApp.filter("offset", function () {
+    return function (input, start) {
+        start = +start;
+        return input.slice(start);
     }
 });
